44c21db3cc22d73162ef43bc87a58eaff4c90862
vikramBhavsar <vikram.bhavsar@somaiya.edu>
2020-06-13 11:03:16 +0530

    Added FBV for Comment Model

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -3,12 +3,28 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    ### Post Model URL Patterns ###
+    # CBV #
     path('posts/',views.PostListView.as_view(),name="post_list"),
     path("/drafts/",views.DraftPostList.as_view(),name="draft_list"),
     path("post/<int:pk>/",views.PostDetailView.as_view(),name="post_detail"),
     path("post/new/",views.CreatePostView.as_view(),name="create_post"),
     path("post/<int:pk>/remove/",views.DeletePostView.as_view(),name="delete_post"),
     path("post/<int:pk>/edit/",views.UpdatePostview.as_view(),name="update_post"),
+
+    # FBV #
+    path("post/<int:pk>/publish/",views.publish_post(),name="publish_post"),
+
+    # Comment Model URL Patterns
+    # CBV #
+
+    # FBV #
+    path('post/<int:pk>/comment/',views.add_comment_to_post(),name="add_comment_to_post"),
+    path('comment/<int:pk>/approve/',views.approve_comment(),name="approve_comment"),
+    path("comment/<int:pk>/remove",views.remove_comment(),name="remove_comment"),
+
+    # Others
     path('about/',views.AboutView.as_view(),name="about_page"),
     path('',views.base,name='base_page'),
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView,ListView,DetailView,CreateView,UpdateView
 from .models import Comment,Post
 from .forms import PostForm,CommentForm
 from django.utils import timezone
+
 
 # Create your views here.
 
@@ -50,3 +52,48 @@ class DraftPostList(LoginRequiredMixin,ListView):
 
     def get_queryset(self):
         return Post.objects.filter(publish_date__isnull=True).order_by("-create_date")
+
+@login_required
+def publish_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect("post_list")
+
+
+###################################################################
+###################################################################
+## Below code is related to Comments
+
+
+@login_required
+def add_comment_to_post(request,pk):
+
+    if request.method == "POST":
+        post = get_object_or_404(Post,pk=pk)
+
+        comment_info = CommentForm(request.POST)
+
+        if comment_info.is_valid():
+            comment_obj = comment_info.save(commit=False)
+            comment_obj.post = post
+            comment_obj.save()
+            return redirect("post_detail",pk=post.pk)
+    else:
+        comment_form = CommentForm()
+
+    return render(request,"blog/comment_form.html",{"comment_form":comment_form})
+
+
+
+@login_required
+def approve_comment(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect("post_detail",pk=comment.post.pk)
+
+@login_required
+def remove_comment(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect("post_detail",pk=post_pk)
